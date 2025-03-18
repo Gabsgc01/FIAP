9636f0211139092054eb64dbec1f70fa4d17db91
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-18 20:09:20 -0300

    aula

--- Computational Thinking with Python/imc.py
+++ Computational Thinking with Python/imc.py
@@ -1,5 +1,5 @@
-height = input('Insira sua altura (exemplo - 1,70): ').replace(',', '.')
-weight = input("Insira seu peso (exemplo - 75,3): ").replace(',', '.')
+height = input float('Insira sua altura (exemplo - 1,70): ').replace(',', '.')
+weight = input float("Insira seu peso (exemplo - 75,3): ").replace(',', '.')
 height = float(height)
 weight = float(weight)
 imc = weight/(height * height)
