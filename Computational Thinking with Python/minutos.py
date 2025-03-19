3bf4daa42bc658743d5a801c79cae376f08b1b3a
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-18 22:18:11 -0300

    aula

--- Computational Thinking with Python/minutos.py
+++ Computational Thinking with Python/minutos.py
@@ -3,5 +3,5 @@ passou = int(input("quantos minutos passaram?:"))
 atual = (minuto_inicial+passou)%60
 nova_hora = (minuto_inicial+passou)//60
 
-print("Agora são", nova_hora, atual)
+print("Agora são", nova_hora,":", atual)
 
