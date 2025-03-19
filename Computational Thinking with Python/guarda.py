4d68d7ebb76ad57c67cb9320d0a20aeff77015ed
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-18 22:16:17 -0300

    aula

---
+++ Computational Thinking with Python/guarda.py
@@ -0,0 +1,12 @@
+velocidade = int(input("Qual velocidade o carro passou?:"))
+
+km = velocidade
+print("Km por Hora Foi", km)
+mh = km*999999
+print("Metros por Hora foi:",mh)
+
+ms = km/3,6
+print("Metros por segundo foi:",ms)
+
+
+
---
+++ Computational Thinking with Python/minutos.py
@@ -0,0 +1,7 @@
+minuto_inicial = 55
+passou = int(input("quantos minutos passaram?:"))
+atual = (minuto_inicial+passou)%60
+nova_hora = (minuto_inicial+passou)//60
+
+print("Agora são", nova_hora, atual)
+
