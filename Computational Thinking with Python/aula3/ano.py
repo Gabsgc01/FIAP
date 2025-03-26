bdd1c526ffcd5a0c4888b29a12b8fd2ded5582ca
Gabriel Ciriaco <gabrielciriaco123@gmail.com>
2025-03-25 22:12:39 -0300

    aula

---
+++ Computational Thinking with Python/aula3/ano.py
@@ -0,0 +1,7 @@
+ano = int(input("Digite o ano que nasceu:")) 
+if ano == 2025:
+    print(("nasceu hoje"))
+elif ano >= 2025:
+    print("Anos invalido para ter nascido")
+elif ano <= 2025:
+    print("Agr foi filho")
--- Computational Thinking with Python/aula3/horas.py
+++ Computational Thinking with Python/aula3/horas.py
@@ -5,8 +5,10 @@ segundos = segundos % 60
 horas = minutos//60
 minutos = minutos%60
 segundos = segundos%60
+dias = horas//24
+horas = horas%24
 
-print(f"passaram-se: {horas}hora(s){minutos}minuto(s){segundos}segundo(s)")
+print(f"passaram-se:{dias}dias {horas}hora(s){minutos}minuto(s){segundos}segundo(s)")
 
 
 
