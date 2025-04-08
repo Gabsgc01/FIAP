ff601b1f3fba3aed515ddd6d09db3bb30d55f868
Gabriel Ciriaco <gabrielciriaco123@gmail.com>
2025-04-07 22:33:42 -0300

    Aula JS

---
+++ Web Development/aula1/aula1.js
@@ -0,0 +1,13 @@
+// alert("Hello Word!")
+//let nome = prompt("Qual pe o seu nome?:", "Insira seu nome aqui");
+//console.log("Teste Console");
+// let nome  :variavel que se modifica
+// const apiKe;y :constante que é fixa
+let nome = prompt("Digite seu nome:")
+let idade = prompt("Digite sua idade:")
+let profissao = prompt("Digite Sua Profissao:")
+
+alert (`Olá ${nome}, tudo bem? Você tem ${idade} anos, e trabalha como ${profissao}`)
+
+//moderjava script
+
