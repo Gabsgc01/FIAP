56f2022df48046e1c97e5053074f4c47f23e53cb
Gabriel Ciriaco <gabrielciriaco123@gmail.com>
2025-04-14 22:12:30 -0300

    Java é complicado

---
+++ Web Development/aula2/aula.js
@@ -0,0 +1,24 @@
+// let nome = "Caio"
+
+// const apiKey = "AOHPH"
+
+// console.log("Mensagem")
+
+// let nome = "outro"
+// if (!nome){
+//     console.log("É voce!")
+// }
+// else if (nome==="Fulano"){
+//     console.log("QUem é voce?")
+// }
+//  else{
+//     console.log("Não é voce ")
+//  }
+
+//  nome === "Caio"? console.log("é voce"): console.log("Não ée voce")
+
+
+let login = prompt("Digite seu Login:")
+let senha = (prompt("Digite sua senha:"))
+
+senha === "123" ? console.log("Login efetuado com sucesso") : console.log("Usuario ou Senha invalidos")
