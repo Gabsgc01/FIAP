c47703cb3a08f7e85bd58b965f808a8b9a75ce9c
Gabriel Ciriaco <gabrielciriaco123@gmail.com>
2025-04-28 22:37:23 -0300

    Java Script

---
+++ Web Development/aula3/app.js
@@ -0,0 +1,63 @@
+// function pegaNome() {
+//    let nome = "Caio"
+//    return nome
+// }
+
+// function exibirNome(){
+//     let resultado = pegaNome()
+//     console.log(resultado)
+// }
+
+// exibirNome()
+
+// function somaNumeros(numero1, numero2 = 0){
+//     let resultado = numero1 + numero2
+//     console.log(resultado)
+// }
+
+// console.log(resultado)
+// somaNumeros(5,19)
+
+// const somaNumeros = (numero1, numero2) => numero1 + numero2
+
+// function pegaIdade() {
+//     let idade = prompt("digite Sua idade")
+//     return idade;
+// }
+
+
+// function pegaNome() {
+//     let nome = prompt("Qual seu nome")
+//     return nome;
+// }
+
+
+// function mostraNome(nome, idade) {
+//     alert(`Olá ${nome}, tudo bem? Você tem ${idade} anos`)
+// }
+
+// function start() {
+//     const nome = pegaNome();
+//     const idade = pegaIdade();
+//     mostraNome(nome, idade);
+// }
+// start();
+nome = "caio"
+console.log(nome.length)
+console.log(nome.toUpperCase())
+console.log(nome.toLowerCase())
+console.log(nome.includes("io"))
+console.log(nome.trim())
+console.log(nome)
+
+let numero = 5.56559
+
+console.log(numero.toFixed(2))
+console.log(isNaN(numero))
+
+console.log(Math.ceil(Math.random() * 11))
+console.log(Math.floor(Math.random() * 11))
+console.log(Math.round(Math.random() * 11))
+
+let dataAtual = new Date()
+console.log(dataAtual)
